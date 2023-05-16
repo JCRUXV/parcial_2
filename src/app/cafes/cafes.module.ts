23bf744e64108e3e84e2572b57b54cb061ad46b5
JCRUXV <78119191+JCRUXV@users.noreply.github.com>
2023-05-16 09:27:14 -0500

    last

--- src/app/cafes/cafes.module.ts
+++ src/app/cafes/cafes.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CafesComponent } from './cafes.component';
+import { CafesListComponent } from '../cafes-list/cafes-list.component';
 
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [CafesComponent],
-  exports: [CafesComponent]
+  declarations: [CafesComponent,CafesListComponent],
+  exports: [CafesComponent,CafesListComponent]
 })
 export class CafesModule { }
